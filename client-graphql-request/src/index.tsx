35bac1c96e634c18d79876886fed130eb15c8776
dycoon <kuroda.dycoon@gmail.com>
2020-05-06 21:30:30 +0900

    add: requesting and rendering to client-graphql-request.

--- client-graphql-request/src/index.tsx
+++ client-graphql-request/src/index.tsx
@@ -1,15 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { request } from "graphql-request";
+
+const url = "http://localhost:4000/graphql";
+
+const query = `
+  query listUsers {
+    allUsers {
+      avatar
+      name
+    }
+  }
+`;
+
+const mutation = `
+  mutation population($count: Int!) {
+    addFakeUsers(count: $count) {
+      githubLogin
+      name
+    }
+  }
+`;
+
+const render = ({ allUsers = [] }) =>
+  ReactDOM.render(
+    <React.StrictMode>
+      <App users={allUsers} />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+
+const requestAndRender = () =>
+  request(url, query).then(render).catch(console.error);
+
+const addUser = () =>
+  request(url, mutation, { count: 1 })
+    .then(requestAndRender)
+    .catch(console.error);
+
+requestAndRender();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
