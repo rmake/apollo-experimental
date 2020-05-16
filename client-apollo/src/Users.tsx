54cf3ddb2aa5c7e31512af94781ded96380f9ac8
dycoon <kuroda.dycoon@gmail.com>
2020-05-16 22:00:19 +0900

    add: mutation

--- client-apollo/src/Users.tsx
+++ client-apollo/src/Users.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Query } from "react-apollo";
+import { Query, Mutation } from "react-apollo";
+import { gql } from "apollo-boost";
 import { ROOT_QUERY } from "./App";
 
 const Users = () => (
@@ -29,6 +30,16 @@ const Users = () => (
   </Query>
 );
 
+const ADD_FAKE_USERS_MUTATION = gql`
+  mutation addFakeUsers($count: Int!) {
+    addFakeUsers(count: $count) {
+      githubLogin
+      name
+      avatar
+    }
+  }
+`;
+
 const UserListItem = ({ name, avatar }: { name: string; avatar: string }) => (
   <li>
     <img src={avatar} width={48} height={48} alt="" />
@@ -47,7 +58,12 @@ const UserList = ({
 }) => (
   <div>
     <p>{count} Users</p>
-    <button onClick={() => refetchUsers}>Refetch Users</button>
+    <button onClick={() => refetchUsers()}>Refetch Users</button>
+    <Mutation mutation={ADD_FAKE_USERS_MUTATION} variables={{ count: 1 }}>
+      {(addFakeUsers: () => void) => (
+        <button onClick={addFakeUsers}>Add Fake Users</button>
+      )}
+    </Mutation>
     <ul>
       {users.map((user: any) => (
         <UserListItem
